094996784fc110762ace2f0c6d0b9c781ef62431
affandiagung <ccoc.20001@gmail.com>
2021-12-28 22:21:01 +0700

    + file index.js CRUD

--- week-2/4/index.js
+++ week-2/4/index.js
@@ -1,12 +1,109 @@
 const express = require("express") // import dari module / package / dependency 
-const apaAja = require('./util/loop') // import dari file yang kita buat sendiri
-const app = express()
+const fs = require("fs");
+const app = express();
 const port = 5000
 
-app.get("/hello", (req, res)=>{
+
+
+app.use(express.json()) 
+app.use(express.urlencoded({ extended: true })) 
+
+// Read - GET Hello World
+app.get("/hello", (req, res)=>{ 
     return res.status(200).json({
         "message" : "Hello World"
     })
 })
 
+//READ
+app.get('/users', (req, res) => {
+    const usersData = getExistData()
+    res.json(usersData)
+})
+
+//CREATE
+app.post('/users/add', (req, res) => {
+    const reqBody = req.body
+    const usersData = getExistData()
+
+    if (reqBody.id == null || reqBody.name == null || reqBody.age == null) {
+        res.status(400).json({ status : 'error', message : 'Data Tidak Lengkap'  })
+    } else {
+        const checkUser = usersData.find(e => e.id == reqBody.id)
+        if(checkUser){
+            res.status(400).json({
+                status : 'error',
+                message : `Users dengan Id ${reqBody.id} sudah ada`
+            })
+        } else {
+            usersData.push(reqBody)
+            saveData(usersData)
+            res.status(200).json({
+                status : 'success',
+                message : `Add UserId ${reqBody.id} Berhasil`
+            })
+        }
+    }
+})
+
+//UPDATE
+app.put('/users/update/:id', (req, res) => {
+    const userId = req.params.id
+    const usersData = getExistData()
+
+    
+    const checkUser = usersData.find(user => user.id == userId)
+    if(checkUser == null){
+        res.status(400).json({
+            status : 'error',
+            message : `Users dengan Id ${userId} tidak ada`
+        })
+    } else {
+        usersData.filter(e => {
+            if(e.id == userId){
+                e.name = req.body.name
+                e.age = req.body.age
+            }
+        })
+        saveData(usersData)
+        res.status(200).json({
+            status : 'success',
+            message : `Update UserId ${userId} Berhasil`
+        })
+    }
+})
+
+//DELETE
+app.delete('/users/delete/:id', (req, res) => {
+    const userId = req.params.id
+    const usersData = getExistData()
+    const filterData = usersData.filter(e => e.id != userId)
+
+    if(filterData.length == usersData.length){
+        res.status(400).json({
+            status : 'error',
+            message : `Users dengan Id ${userId} tidak ada`
+        })
+    } else {
+        saveData(filterData)
+        res.status(200).json({
+            status : 'success',
+            message : `Delete UserId ${userId} Berhasil`
+        })
+    }
+})
+
+//Get Data from users.json
+const getExistData = () => {
+    const jsonData = fs.readFileSync('users.json')
+    return JSON.parse(jsonData)  
+}
+
+//Save Data to users.json
+const saveData = (data) => {    
+    const stringifyData = JSON.stringify(data)
+    fs.writeFileSync('users.json', stringifyData)
+}
+
+//PORT Listen
 app.listen(port, ()=> console.log("Project is running at port", port))
--- week-2/4/util/loop.js
+++
@@ -1,7 +0,0 @@
-const looping = ()=>{
-    for(let i = 0; i <10; i++){
-        console.log(i)
-    }
-}
-
-module.exports = looping
