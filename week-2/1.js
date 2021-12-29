fd8423ba585f0ca69c652c530c9ab9dfaf3626eb
Hammid Muammar <spinxclopz21@gmail.com>
2021-12-30 00:05:47 +0700

    nomor 1

--- week-2/1.js
+++ week-2/1.js
@@ -1,16 +1,18 @@
 const duplicateCount = (input) => {
+    input = input.toLowerCase();
     data = {},
     result = 0;    
     for( i = 0; i < input.length; i++){   
-      if(!data[input[i]]){
-          data[input[i]] = 1;
+      if(!data[input[i]]){ 
+          data[input[i]] = 1; // 'a': 1
       } 
-      else if(data[input[i]] < 2) {
+      else if(data[input[i]] >= 1) {
           data[input[i]]+=1;
-          result++;
+          result+=1;
       }    
     }
     return result;
 }
 
-console.log(duplicateCount('AAbbCCdde'))
+console.log(duplicateCount('AAbbCcd'))
+console.log(duplicateCount('AAbbCcddEEee')) // :(
